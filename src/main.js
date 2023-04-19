33f0fee9590e0771cbf13477487b68042a6b5cc0
Koreyoshi2023 <2323148223@qq.com>
2023-04-19 10:19:47 +0800

    init gshop

---
+++ src/main.js
@@ -0,0 +1,16 @@
+/*
+入口JS
+ */
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  render: h => h(App),
+  router,//使用上vue-router
+
+})
